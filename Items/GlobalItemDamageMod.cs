d311dfb0bd742f04fcbd6df9895b76377f62ccc3
Xahlicem <xahlicem@gmail.com>
2017-08-24 09:33:04 -0700

    Displays crit, knockback and mana use.

--- Items/GlobalItemDamageMod.cs
+++ Items/GlobalItemDamageMod.cs
@@ -13,7 +13,7 @@ using Terraria.ModLoader.IO;
 namespace XItemStats.Items {
     public class GlobalItemStatsMod : GlobalItem {
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips) {
-            if (!(item.damage > 0)) return;
+            Player player = Main.player[item.owner];
             for (int i = 0; i < tooltips.Count; i++) {
                 if (tooltips[i].Name.Equals("Damage")) {
                     string[] text = tooltips[i].text.Split(' ');
@@ -21,8 +21,26 @@ namespace XItemStats.Items {
                     baseItem.CloneDefaults(item.type);
                     int damage = int.Parse(text[0]);
                     damage -= baseItem.damage;
+
                     if (damage != 0) {
                         tooltips[i].text = text[0] + "(" + ((damage > 0) ? "+" : "-") + Math.Abs(damage) + ")";
+                        for (int j = 1; j < text.Length; j++)
+                            tooltips[i].text += " " + text[j];
+                    }
+                }
+                if (tooltips[i].Name.Equals("CritChance")) {
+                    string[] text = tooltips[i].text.Split(' ');
+                    Item baseItem = new Item();
+                    baseItem.CloneDefaults(item.type);
+                    int crit = -4;
+                    if (item.magic) crit += player.magicCrit;
+                    else if (item.ranged) crit += player.rangedCrit;
+                    else if (item.thrown) crit += player.thrownCrit;
+                    else if (item.melee) crit += player.meleeCrit;
+                    crit += (item.crit - baseItem.crit);
+
+                    if (crit != 0) {
+                        tooltips[i].text = text[0] + "(" + ((crit > 0) ? "+" : "-") + Math.Abs(crit) + ")";
                         for (int j = 1; j < text.Length; j++)
                             tooltips[i].text += " " + text[j];
                     }
@@ -35,16 +53,43 @@ namespace XItemStats.Items {
                     int speedMod = 0;
                     if (item.melee) {
                         speed = item.useAnimation - 1;
-                        speed = (int)((float) speed * Main.player[item.owner].meleeSpeed);
+                        speed = (int)((float) speed * player.meleeSpeed);
                         speedMod = speed - (baseItem.useAnimation - 1);
                     } else {
                         speed = item.useTime;
                         speedMod = speed - baseItem.useTime;
                     }
+
                     tooltips[i].text = speed.ToString();
                     if (speedMod != 0) tooltips[i].text += "(" + ((speedMod > 0) ? "+" : "-") + Math.Abs(speedMod) + ")";
-                    for (int j = 1; j < text.Length; j++)
-                        tooltips[i].text += " " + text[j];
+                    for (int j = 0; j < text.Length; j++)
+                        tooltips[i].text += ((j == 0) ? " (" : " ") + text[j] + ((j == text.Length - 2) ? ")" : "");
+                }
+                if (tooltips[i].Name.Equals("Knockback")) {
+                    string[] text = tooltips[i].text.Split(' ');
+                    Item baseItem = new Item();
+                    baseItem.CloneDefaults(item.type);
+                    float knockBack = item.knockBack;
+                    float knockBackMod = knockBack - baseItem.knockBack;
+
+                    tooltips[i].text = knockBack.ToString();
+                    if (knockBackMod != 0) tooltips[i].text += "(" + ((knockBackMod > 0) ? "+" : "-") + Math.Abs(knockBackMod) + ")";
+                    for (int j = 0; j < text.Length; j++)
+                        tooltips[i].text += ((j == 0) ? " (" : " ") + text[j] + ((j == text.Length - 2) ? ")" : "");
+
+                }
+                if (tooltips[i].Name.Equals("UseMana")) {
+                    string[] text = tooltips[i].text.Split(' ');
+                    Item baseItem = new Item();
+                    baseItem.CloneDefaults(item.type);
+                    int damage = int.Parse(text[1]);
+                    damage -= baseItem.mana;
+
+                    if (damage != 0) {
+                        tooltips[i].text = text[0] + " " + text[1] + "(" + ((damage > 0) ? "+" : "-") + Math.Abs(damage) + ")";
+                        for (int j = 2; j < text.Length; j++)
+                            tooltips[i].text += " " + text[j];
+                    }
                 }
                 //tooltips[i].text += " " + tooltips[i].Name;
             }
